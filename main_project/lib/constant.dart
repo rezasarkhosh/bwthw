1d25a50c9ff80d206042673ecefbb3da7a2e23c6
Rezas <99653603+rezasarkhosh@users.noreply.github.com>
2024-07-22 11:30:51 +0200

    Update constant.dart

--- main_project/lib/constant.dart
+++ main_project/lib/constant.dart
@@ -1,3 +1,3 @@
-const MONGO_URL = "mongodb+srv://Rezas:12345678A@atlascluster.42rv3bo.mongodb.net/Rezas?retryWrites=true&w=majority&appName=AtlasCluster";
+const MONGO_URL = "";
 const Users= "users";
 const Donors= "Donors";
